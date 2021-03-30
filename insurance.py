da475893ea55df8e75a9212f70e01ed2aebf453f
carlosishimwe <72576437+carlosishimwe@users.noreply.github.com>
2021-03-30 11:16:30 +0200

    Update insurance.py

--- insurance.py
+++ insurance.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from PIL import Image
 from pandas_profiling import ProfileReport
 from pycaret.regression import load_model, predict_model
